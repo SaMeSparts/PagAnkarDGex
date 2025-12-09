e4af957822e00a0d01935f693c7e00811e2e89d8
SaMeSparts <132626666+SaMeSparts@users.noreply.github.com>
2025-12-09 15:46:06 -0600

    Se añadio el dominio

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/PagAnkarDGex/', // 👈 muy importante: el nombre EXACTO de tu repositorio
+  base: '/', // 👈 muy importante: el nombre EXACTO de tu repositorio
 })
